1689b177f90a64beded72f916859a32e4d5d64b5
PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>
2020-10-26 14:16:02 +0100

    Update PatriikInstaller.lua

--- PatriikInstaller.lua
+++ PatriikInstaller.lua
@@ -9,7 +9,7 @@ print('---------------------------\nPatriikInstaller '..version..'\n------------
 os.sleep(3)
 while true do
     local file = io.read()
-    local wgetItem = 'wget "https://raw.githubusercontent.com/PatriikPlays/PatriikInstaller/main/downloads/"..file..".lua" /home/'..file..'.lua -f'
+    local wgetItem = tostring('wget "https://raw.githubusercontent.com/PatriikPlays/PatriikInstaller/main/downloads/"..file..".lua" /home/'..file..'.lua -f')
     local success,err = pcall(function()
         os.execute(wgetItem)
     end)
