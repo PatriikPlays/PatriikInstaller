8fc1c3ef21ea00a3a32da3b2159bf327aedb368e
PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>
2020-10-26 13:03:37 +0100

    Update PatriikInstaller.lua

--- PatriikInstaller.lua
+++ PatriikInstaller.lua
@@ -1,1 +1,5 @@
-os.execute('wget "https://raw.githubusercontent.com/PatriikPlays/PatriikInstaller/main/downloads/test.lua" test.lua -f')
+local version = 1.0
+term.clear()
+print('---------------------------\nPatriikInstaller '..version..'\n---------------------------\n ')
+os.sleep(3)
+os.execute('wget "https://raw.githubusercontent.com/PatriikPlays/PatriikInstaller/main/downloads/test.lua" /home/test.lua -f')
