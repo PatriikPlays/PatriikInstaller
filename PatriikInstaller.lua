795a7743cc599024a4a6acbb01493d4f5deabd59
PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>
2020-10-26 13:18:04 +0100

    Update PatriikInstaller.lua

--- PatriikInstaller.lua
+++ PatriikInstaller.lua
@@ -1,5 +1,11 @@
 local version = 1.0
-term.clear()
+local component = require('component')
+local gpu = component.gpu
+local screenWidth,screenHeight = gpu.getResolution()
+gpu.fill(1,1,screenWidth,screenHeight,' ')
+gpu.setForeground(0x000000)
+gpu.setBackground(0xFFFFFF)
+
 print('---------------------------\nPatriikInstaller '..version..'\n---------------------------\n ')
 os.sleep(3)
 os.execute('wget "https://raw.githubusercontent.com/PatriikPlays/PatriikInstaller/main/downloads/test.lua" /home/test.lua -f')
