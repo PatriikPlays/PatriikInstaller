ae49cd61843be1971809b25d02c1a87ac84654ad
PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>
2020-12-10 19:23:52 +0100

    Update PatriikInstaller.lua

--- PatriikInstaller.lua
+++ PatriikInstaller.lua
@@ -1,17 +1,1 @@
-local version = '1.0'
-local component = require('component')
-local gpu = component.gpu
-local term = require('term')
-term.clear()
-term.setCursor(1,1)
 
-print('---------------------------\nPatriikInstaller '..version..'\n---------------------------\n ')
-os.sleep(3)
-while true do
-    local file = io.read()
-
-    if file == 'test' then
-        os.execute('wget "https://raw.githubusercontent.com/PatriikPlays/PatriikInstaller/main/downloads/test.lua" /bin/test.lua -f')
-    end
-    
-end
